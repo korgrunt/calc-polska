ab7ebca49072ab37e42dcf15960df959ba02b7fc
korgrunt <naim.aouaichia@gmail.com>
2023-10-07 11:47:41 +0200

    aqMxQUXbGxEDHx3T

--- CalcUI.java
+++ CalcUI.java
@@ -1,13 +1,22 @@
 import java.lang.*;
 import java.io.*;
 
-class CalcUI {
+public class CalcUI {
 
-   public CalcUI(){
+    public static void main(String[] args)
+        throws IOException
+    {
+        
 
-   }
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in)
+                );
 
-    
+        String name = reader.readLine();
+        System.out.println(name);
+    }
+
+
 
 
 }   
